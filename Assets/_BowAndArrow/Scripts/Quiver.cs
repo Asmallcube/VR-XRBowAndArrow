6b44838e015e4eac8c9e61ed5bf24e4d0f364486
Angus Lisle <51740248+Asmallcube@users.noreply.github.com>
2020-10-22 14:50:35 +1000

    Revert "Made it so you can pick up and noch arrow"
    This reverts commit 9cf1c78d78a950b0a2bca417bf84278a0c63e730.

--- Assets/_BowAndArrow/Scripts/Quiver.cs
+++ Assets/_BowAndArrow/Scripts/Quiver.cs
@@ -3,44 +3,33 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Quiver : XRSocketInteractor
 {
-    public GameObject arrowPrefab = null;
-    private Vector3 attachOffSet = Vector3.zero;
     protected override void Awake()
     {
         base.Awake();
-        CreateAndSelectArrow();
-        SetAttachOffset();
     }
 
     protected override void OnSelectExit(XRBaseInteractable interactable)
     {
         base.OnSelectExit(interactable);
-        CreateAndSelectArrow();
     }
 
     private void CreateAndSelectArrow()
     {
-        Arrow arrow = CreateArrow();
-        SelectArrow(arrow);
+
     }
 
     private Arrow CreateArrow()
     {
-        GameObject arrowObject = Instantiate(arrowPrefab, transform.position - attachOffSet, transform.rotation);
-        return arrowObject.GetComponent<Arrow>();
+        return null;
     }
 
     private void SelectArrow(Arrow arrow)
     {
-        OnSelectEnter(arrow);
-        arrow.OnSelectEnter(this);
+
     }
 
     private void SetAttachOffset()
     {
-        if (selectTarget is XRGrabInteractable interactable)
-        {
-            attachOffSet = interactable.attachTransform.localPosition;
-        }
+
     }
 }
